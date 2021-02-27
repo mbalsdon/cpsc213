1b6e4c096954be3ace5b22689bcc8036ba4cb451
Mathew Balsdon <60245574+spreadnuts@users.noreply.github.com>
2021-02-26 22:26:15 -0800

    implement all free list allocator functions

--- asgt5/implicitfreelist/implicit.c
+++ asgt5/implicitfreelist/implicit.c
@@ -111,8 +111,26 @@ static int is_within_heap_range(struct heap *h, void *addr) {
  * the coalesced block.
  */
 static void *coalesce(struct heap *h, void *first_block_start) {
+
+  if (is_last_block(h, first_block_start)) return first_block_start;
   
-  /* TO BE COMPLETED BY THE STUDENT. */
+  int *next_block_start = get_next_block(first_block_start);
+
+  if (block_is_in_use(first_block_start) || block_is_in_use(next_block_start)) {
+
+    return first_block_start;
+
+  } else {
+
+    /* Compute size of coalesced block */
+    int first_block_size = get_block_size(first_block_start);
+    int next_block_size = get_block_size(next_block_start);
+    int coalesced_block_size = first_block_size + next_block_size;
+    set_block_header(first_block_start, coalesced_block_size, 0);
+
+    return first_block_start;
+  }
+  
   return NULL;
 }
 
@@ -123,9 +141,20 @@ static void *coalesce(struct heap *h, void *first_block_start) {
  * of HEADER_SIZE.
  */
 static int get_size_to_allocate(int user_size) {
-  
-  /* TO BE COMPLETED BY THE STUDENT. */
-  return 0;
+
+  if (user_size == 0) {
+    
+    return HEADER_SIZE * 3;
+
+  } else if (user_size % 4 == 0) {
+
+    return user_size + HEADER_SIZE * 2;
+
+  } else {
+
+    /* Pad requested size so that it is aligned */
+    return HEADER_SIZE - (user_size % HEADER_SIZE) + user_size + HEADER_SIZE * 2;
+  }
 }
 
 /*
@@ -139,8 +168,22 @@ static int get_size_to_allocate(int user_size) {
  */
 static void *split_and_mark_used(struct heap *h, void *block_start, int needed_size) {
 
-  /* TO BE COMPLETED BY THE STUDENT. */
-  return NULL;
+  int block_size = get_block_size(block_start);
+  
+  if (block_size - needed_size >= HEADER_SIZE * 3) {
+
+    set_block_header(block_start, needed_size, 1);
+
+    /* Create second, free block */
+    int free_block_size = block_size - needed_size;
+    void *next_block_start = get_next_block(block_start);
+    set_block_header(next_block_start, free_block_size, 0);
+
+  } else {
+
+    set_block_header(block_start, block_size, 1);
+  }
+  return get_payload(block_start);
 }
 
 /*
@@ -169,7 +212,16 @@ struct heap *heap_create(unsigned int size)
  */
 void myfree(struct heap *h, void *payload) {
   
-  /* TO BE COMPLETED BY THE STUDENT. */
+  /* Set header A/F to 0, effectively freeing it */
+  void *block_start = get_block_start(payload);
+  set_block_header(block_start, get_block_size(block_start), 0);
+  coalesce(h, block_start);
+
+  /* Attempt to coalesce previous block */
+  if (!is_first_block(h, block_start)) {
+    void *prev_block_start = get_previous_block(block_start);
+    coalesce(h, prev_block_start);
+  }
 }
 
 /*
@@ -178,7 +230,22 @@ void myfree(struct heap *h, void *payload) {
  * block large enough to satisfy the request exists.
  */
 void *mymalloc(struct heap *h, unsigned int user_size) {
-  
-  /* TO BE COMPLETED BY THE STUDENT. */
+
+  void *current_block_size = h->start;
+  int is_big_enough;
+  int is_allocated;
+
+  while (is_within_heap_range(h, current_block_size)) {
+
+    is_big_enough = get_payload_size(current_block_size) >= user_size;
+    is_allocated = block_is_in_use(current_block_size);
+
+    if (is_big_enough && !is_allocated) {
+      split_and_mark_used(h, current_block_size, get_size_to_allocate(user_size));
+      return get_payload(current_block_size);
+    }
+    current_block_size = get_next_block(current_block_size);
+  }
+
   return NULL;
 }
